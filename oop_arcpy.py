93388d1d316e3450f655725dae82d45d61109e90
elpahlevi <elpahlevi98@gmail.com>
2019-09-17 18:09:38 +0700

    Added calculate brightness temperature function

--- oop_arcpy.py
+++ oop_arcpy.py
@@ -76,13 +76,62 @@ class gis():
         ndvi_calculate = Float(nir-red)/Float(nir+red)
         ndvi_calculate.save(ndvi_output)
         print("Selesai memroses NDVI")
-            
+
+    def prop_ems_veg(self):
+        ENV.workspace = self.outDIR+self.ndvi_image
+        raster_ndvi = arcpy.ListRasters("","TIF")
+        for ras_ndvi in raster_ndvi:
+            ndvi_raster = arcpy.sa.Raster(ras_ndvi)
+        ndvi_min = -0.6998
+        ndvi_max = 0.870175
+        ems_output = "D:/arcpy/results/ems/ems_kulonprogo.TIF"
+        proportion_vegetation = Square((ndvi_raster-ndvi_min)/(ndvi_max-ndvi_min))
+        ems = 0.004*proportion_vegetation + 0.986
+        ems.save(ems_output)
+        print("Selesai menghitung Emisivitas")
+
+    def t_brightness(self):
+        ENV.workspace = self.workDIR+self.satellite_image
+        kulonprogo_shape = self.outDIR+self.shapeFile+"/kulonprogo.shp"
+        raster_sr_list = ["*B10*", "*B11*"]
+        image_list = []
+        rad_mult = 0.0003342
+        rad_add = 0.1
+        b10_clip = "D:/arcpy/results/sr/b10_kulonprogo.TIF"
+        b11_clip = "D:/arcpy/results/sr/b11_kulonprogo.TIF"
+        tb_output = "D:/arcpy/results/sr/tb_kulonprogo.TIF"
+        for ras_sr in raster_sr_list:
+            raster_src = arcpy.ListRasters(ras_sr, "TIF")
+            image_list.extend(raster_src)
+        #clip image
+        b10 = arcpy.sa.Raster(image_list[0])
+        arcpy.Clip_management(b10, "#", b10_clip , kulonprogo_shape, "0", "ClippingGeometry", "NO_MAINTAIN_EXTENT")
+        b11 = arcpy.sa.Raster(image_list[1])
+        arcpy.Clip_management(b11, "#", b11_clip , kulonprogo_shape, "0", "ClippingGeometry", "NO_MAINTAIN_EXTENT")
+        #calculate spectral radiance
+        b10_raster = arcpy.sa.Raster(b10_clip)
+        b11_raster = arcpy.sa.Raster(b11_clip)
+        sr_10 = (rad_mult * b10_raster) + rad_add
+        sr_11 = (rad_mult * b11_raster) + rad_add
+        #calculate brightness temperature
+        K1_CONSTANT_BAND_10 = 774.8853
+        K2_CONSTANT_BAND_10 = 1321.0789
+        K1_CONSTANT_BAND_11 = 480.8883
+        K2_CONSTANT_BAND_11 = 1201.1442
+
+        tb_10 = K2_CONSTANT_BAND_10/Ln((K1_CONSTANT_BAND_10/sr_10)+1)
+        tb_11 = K2_CONSTANT_BAND_11/Ln((K1_CONSTANT_BAND_11/sr_11)+1)
+        #calculate average brightness temperature
+        tb_ave = (tb_10+tb_11)/2
+        tb_ave.save(tb_output)
 
 #run the function
 # run_correction = gis().rad_correction()
 # run_clip = gis().clip_image()
 #composite_clip = gis().comp_image()
 #ndvi_run = gis().ndvi()
+#ems_run = gis().prop_ems_veg()
+sr_run = gis().t_brightness()
 
 
     
